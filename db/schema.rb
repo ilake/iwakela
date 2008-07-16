99a10b20e0ed934a11f705f19032ab86177463f6
contagion <contagion@gmail.com>
2008-07-16 12:43:50 +0800

    remove ferret, do not track config/database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -9,41 +9,41 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080708073042) do
+ActiveRecord::Schema.define(:version => 20080708135905) do
 
   create_table "calls", :force => true do |t|
-    t.integer  "demander_id", :limit => 11
-    t.integer  "accepter_id", :limit => 11
+    t.integer  "demander_id"
+    t.integer  "accepter_id"
     t.string   "title"
     t.datetime "time"
   end
 
   create_table "chats", :force => true do |t|
-    t.integer  "user_id",    :limit => 11
+    t.integer  "user_id"
     t.datetime "created_at"
     t.text     "content"
-    t.integer  "group_id",   :limit => 11
+    t.integer  "group_id"
   end
 
   create_table "comments", :force => true do |t|
     t.datetime "created_at"
     t.text     "content"
-    t.integer  "record_id",   :limit => 11, :null => false
+    t.integer  "record_id",   :null => false
     t.string   "record_type"
-    t.integer  "user_id",     :limit => 11
+    t.integer  "user_id"
   end
 
   create_table "forums", :force => true do |t|
     t.string   "subject"
     t.text     "content"
     t.datetime "created_at"
-    t.integer  "user_id",        :limit => 11,                :null => false
-    t.integer  "comments_count", :limit => 11, :default => 0
-    t.integer  "group_id",       :limit => 11
+    t.integer  "user_id",                       :null => false
+    t.integer  "comments_count", :default => 0
+    t.integer  "group_id"
   end
 
   create_table "goals", :force => true do |t|
-    t.integer "user_id", :limit => 11
+    t.integer "user_id"
     t.boolean "choosed"
     t.boolean "done"
     t.string  "name"
@@ -54,26 +54,26 @@ ActiveRecord::Schema.define(:version => 20080708073042) do
     t.string   "name"
     t.text     "board"
     t.text     "condition"
-    t.integer  "user_num",   :limit => 11
-    t.integer  "state",      :limit => 11, :default => 0
+    t.integer  "user_num"
+    t.integer  "state",      :default => 0
     t.datetime "created_at"
-    t.integer  "owner_id",   :limit => 11
+    t.integer  "owner_id"
   end
 
   create_table "mugshots", :force => true do |t|
-    t.integer  "user_id",      :limit => 11
+    t.integer  "user_id"
     t.datetime "created_at"
-    t.integer  "parent_id",    :limit => 11
+    t.integer  "parent_id"
     t.string   "content_type"
     t.string   "filename"
     t.string   "thumbnail"
-    t.integer  "size",         :limit => 11
-    t.integer  "width",        :limit => 11
-    t.integer  "height",       :limit => 11
+    t.integer  "size"
+    t.integer  "width"
+    t.integer  "height"
   end
 
   create_table "profiles", :force => true do |t|
-    t.integer "user_id",      :limit => 11
+    t.integer "user_id"
     t.text    "others"
     t.string  "sex"
     t.text    "birth"
@@ -85,19 +85,19 @@ ActiveRecord::Schema.define(:version => 20080708073042) do
     t.string  "interest"
     t.string  "photo"
     t.string  "connect"
-    t.boolean "email_weekly",               :default => true
+    t.boolean "email_weekly", :default => true
   end
 
   create_table "records", :force => true do |t|
     t.string   "todo_name"
     t.datetime "todo_time"
     t.datetime "todo_target_time"
-    t.integer  "user_id",          :limit => 11,                   :null => false
+    t.integer  "user_id",                            :null => false
     t.text     "content"
-    t.boolean  "success",                        :default => true
+    t.boolean  "success",          :default => true
     t.text     "goal"
     t.text     "com_goal"
-    t.integer  "state",            :limit => 11
+    t.integer  "state"
   end
 
   create_table "sessions", :force => true do |t|
@@ -106,28 +106,28 @@ ActiveRecord::Schema.define(:version => 20080708073042) do
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
 
   create_table "statuses", :force => true do |t|
-    t.integer  "user_id",                :limit => 11
-    t.boolean  "fight",                                :default => true
-    t.integer  "state",                  :limit => 11, :default => 0
+    t.integer  "user_id"
+    t.boolean  "fight",                  :default => true
+    t.integer  "state",                  :default => 0
     t.datetime "average"
     t.float    "success_rate"
-    t.integer  "continuous_num",         :limit => 11
-    t.integer  "num",                    :limit => 11
+    t.integer  "continuous_num"
+    t.integer  "num"
     t.datetime "last_record_created_at"
     t.datetime "group_join_date"
-    t.float    "attendance",                           :default => 0.0
-    t.integer  "diff",                   :limit => 11
+    t.float    "attendance"
+    t.integer  "diff"
   end
 
   create_table "targets", :force => true do |t|
-    t.integer  "user_id",          :limit => 11, :null => false
+    t.integer  "user_id",          :null => false
     t.datetime "todo_target_time"
-    t.integer  "week",             :limit => 11
-    t.integer  "month",            :limit => 11
+    t.integer  "week"
+    t.integer  "month"
   end
 
   create_table "users", :force => true do |t|
@@ -141,9 +141,9 @@ ActiveRecord::Schema.define(:version => 20080708073042) do
     t.string   "reset_password_code"
     t.datetime "reset_password_code_until"
     t.string   "yahoo_userhash"
-    t.integer  "group_id",                  :limit => 11
+    t.integer  "group_id"
     t.string   "group_nickname"
-    t.string   "time_zone",                               :default => "Taipei"
+    t.string   "time_zone",                 :default => "Taipei"
   end
 
 end
