3c75da6ceac8d541ddae02806cec7965e105a7ff
contagion <contagion@gmail.com>
2008-07-16 20:51:58 +0800

    some style fix

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
-  map.connect '', :controller => 'main', :action => 'index'
+  map.home '', :controller => 'main', :action => 'index'
+
 
   map.connect "main/reset_password/:reset_code",
                 :conditions => { :method => :get },
