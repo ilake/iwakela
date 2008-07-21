62d7e8ba4cd7cc2c833aea10d70e011e2b9b3bf9
lake <lake.ilakela+github@gmail.com>
2008-07-21 15:58:02 +0800

    add medium size

--- app/models/mugshot.rb
+++ app/models/mugshot.rb
@@ -2,7 +2,7 @@ class Mugshot < ActiveRecord::Base
   has_attachment :content_type => :image, 
     :storage => :file_system, 
     :resize_to => '125x125>',
-    :thumbnails => { :small => '24x24>' }
+    :thumbnails => { :medium => '60x60>', :small => '24x24>' }
 
   validates_as_attachment
 
