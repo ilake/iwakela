c8e3ece4d51d57c4dcf6a9b692156b6c0b3c6220
lake <lake.ilakela+github@gmail.com>
2008-07-24 17:14:35 +0800

    fix the photo size and chart

--- app/helpers/all_helper.rb
+++ app/helpers/all_helper.rb
@@ -89,7 +89,7 @@ module AllHelper
   def link_to_user(u, text, cls=nil, img=false)
     str = "<span class='user_photo_small'>"
     if u.mugshot && img
-      str << "#{link_to image_tag(u.mugshot.public_filename(:small)), {:controller => 'member', :action => 'list', :id => u}}</span>"
+      str << "#{link_to image_tag(u.mugshot.public_filename(:small), :size =>'30x30'), {:controller => 'member', :action => 'list', :id => u}}</span>"
     elsif img
       str << "#{link_to image_tag("penguin_small.jpg"),{ :controller => 'member', :action => 'list', :id => u}}</span>"
     end
