d579fccf518a94d893e204a853a4932200dc7520
lake <lake.ilakela+github@gmail.com>
2008-08-08 06:21:36 +0800

    group attendence

--- app/models/group.rb
+++ app/models/group.rb
@@ -35,7 +35,7 @@ class Group < ActiveRecord::Base
         g.members.find(:all).each do |m|
         total_join_days = ((now.at_beginning_of_day - m.status.group_join_date)/86400).to_i
         total_join_days = 1 if total_join_days.zero?
-        record_nums =  m.records.count(:all, :conditions => ['todo_time > ? ', m.status.group_join_date.tomorrow])
+        record_nums =  m.records.wake.count(:all, :conditions => ['todo_time > ? ', m.status.group_join_date.tomorrow])
         attendance = record_nums/total_join_days.to_f
         m.status.update_attribute(:attendance, attendance*100)
       end
