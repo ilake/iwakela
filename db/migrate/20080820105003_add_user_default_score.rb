33c089ed3909a85444d432edf749319a95ed4872
lake <lake.ilakela+github@gmail.com>
2008-08-21 11:40:02 +0800

    add the default score

--- db/migrate/20080820105003_add_user_default_score.rb
+++ db/migrate/20080820105003_add_user_default_score.rb
@@ -4,9 +4,9 @@ class AddUserDefaultScore < ActiveRecord::Migration
       u.scores.create(:name => 'wake', :value => 1)
       u.scores.create(:name => 'sleep', :value => 2)
 
-      #status  = u.status
-      #scores = u.scores
-      #u.records.set_score(status, scores)
+      status  = u.status
+      scores = u.scores
+      u.records.set_score(status, scores)
     end
   end
 
