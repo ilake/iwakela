b6e4feeaf3156447aad77c2fea6fe0efe0f82a63
lake <lake.ilakela+github@gmail.com>
2008-08-22 14:42:48 +0800

    time zone wake time bugs

--- db/migrate/20080822055431_fix_the_wake_time_bug.rb
+++ db/migrate/20080822055431_fix_the_wake_time_bug.rb
@@ -1,38 +1,38 @@
 class FixTheWakeTimeBug < ActiveRecord::Migration
   def self.up
-    User.find(:all, :conditions => {:id => 1..72}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attributes(:todo_time => 8.hours.since(r.todo_time), :todo_target_time => 8.hours.since(r.todo_target_time))
-          else
-            r.update_attribute(:todo_time, 8.hours.since(r.todo_time))
-          end
-        end
-      end
-    end
-
-    User.find(:all, :conditions => {:id => 74..78}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attributes(:todo_time => 8.hours.since(r.todo_time), :todo_target_time => 8.hours.since(r.todo_target_time))
-          else
-            r.update_attribute(:todo_time, 8.hours.since(r.todo_time))
-          end
-        end
-      end
-    end
-
-    User.find(:all, :conditions => {:id => 73}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attribute(:todo_target_time , 8.hours.since(r.todo_target_time))
-          end
-        end
-      end
-    end
+#    User.find(:all, :conditions => {:id => 1..72}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attributes(:todo_time => 8.hours.since(r.todo_time), :todo_target_time => 8.hours.since(r.todo_target_time))
+#          else
+#            r.update_attribute(:todo_time, 8.hours.since(r.todo_time))
+#          end
+#        end
+#      end
+#    end
+#
+#    User.find(:all, :conditions => {:id => 74..78}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attributes(:todo_time => 8.hours.since(r.todo_time), :todo_target_time => 8.hours.since(r.todo_target_time))
+#          else
+#            r.update_attribute(:todo_time, 8.hours.since(r.todo_time))
+#          end
+#        end
+#      end
+#    end
+#
+#    User.find(:all, :conditions => {:id => 73}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attribute(:todo_target_time , 8.hours.since(r.todo_target_time))
+#          end
+#        end
+#      end
+#    end
 #    Record.find(:all).each do |r|
 #      if r.todo_target_time
 #        r.update_attributes(:todo_time => 8.hours.since(r.todo_time), :todo_target_time => 8.hours.since(r.todo_target_time))
@@ -61,38 +61,38 @@ class FixTheWakeTimeBug < ActiveRecord::Migration
   end
 
   def self.down
-    User.find(:all, :conditions => {:id => 1..72}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attributes(:todo_time => r.todo_time.ago(8.hours), :todo_target_time => r.todo_target_time.ago(8.hours))
-          else
-            r.update_attribute(:todo_time, r.todo_time.ago(8.hours))
-          end
-        end
-      end
-    end
-
-    User.find(:all, :conditions => {:id => 74..78}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attributes(:todo_time => r.todo_time.ago(8.hours), :todo_target_time => r.todo_target_time.ago(8.hours))
-          else
-            r.update_attribute(:todo_time, r.todo_time.ago(8.hours))
-          end
-        end
-      end
-    end
-
-    User.find(:all, :conditions => {:id => 73}).each do |u|
-      u.records.each do |r|
-        if r.todo_time < Time.parse("20080603")
-          if r.todo_target_time
-            r.update_attribute(:todo_target_time , r.todo_target_time.ago(8.hours))
-          end
-        end
-      end
-    end
+#    User.find(:all, :conditions => {:id => 1..72}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attributes(:todo_time => r.todo_time.ago(8.hours), :todo_target_time => r.todo_target_time.ago(8.hours))
+#          else
+#            r.update_attribute(:todo_time, r.todo_time.ago(8.hours))
+#          end
+#        end
+#      end
+#    end
+#
+#    User.find(:all, :conditions => {:id => 74..78}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attributes(:todo_time => r.todo_time.ago(8.hours), :todo_target_time => r.todo_target_time.ago(8.hours))
+#          else
+#            r.update_attribute(:todo_time, r.todo_time.ago(8.hours))
+#          end
+#        end
+#      end
+#    end
+#
+#    User.find(:all, :conditions => {:id => 73}).each do |u|
+#      u.records.each do |r|
+#        if r.todo_time < Time.parse("20080603")
+#          if r.todo_target_time
+#            r.update_attribute(:todo_target_time , r.todo_target_time.ago(8.hours))
+#          end
+#        end
+#      end
+#    end
   end
 end
