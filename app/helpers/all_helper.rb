b89c08c28fe11008d4baa2b6de4971e352f3e9e7
lake <lake.ilakela+github@gmail.com>
2008-08-28 23:31:30 +0800

    change the default score nil status

--- app/helpers/all_helper.rb
+++ app/helpers/all_helper.rb
@@ -255,7 +255,7 @@ module AllHelper
 
   def score_state(user, size='128')
     score = user.status.score
-    score ||= 0
+    score ||= -84
     SCORE_STATE.each_with_index do |a,i|
       if score > SCORE_STATE.at(i)
         files = Dir.entries("#{RAILS_ROOT}/public/images/score/#{i}")
