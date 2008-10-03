a0025346d98d51086988b5563dcb366e437ee649
lake <lake.ilakela+github@gmail.com>
2008-10-03 22:40:57 +0800

    ignore the unknownaction

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,4 @@
 HoptoadNotifier.configure do |config|
   config.api_key = '9d5282c83ba521438fc796e05e71d40e'
+  config.ignore_only  = (HoptoadNotifier::IGNORE_DEFAULT.dup << 'ActionController::UnknownAction')
 end
