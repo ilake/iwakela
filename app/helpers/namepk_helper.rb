47c5756c1446f1efc287452c1a10349752bd68c8
lake <lake.ilakela+github@gmail.com>
2008-10-07 16:30:18 +0800

    加上狀態圖

--- app/helpers/namepk_helper.rb
+++ app/helpers/namepk_helper.rb
@@ -1,2 +1,34 @@
 module NamepkHelper
+  def attr_string(attrs)
+    status = attrs.inject({}){|h,k| h[k[:name]] = k[:value]; h }
+  end
+
+  def pk_charts(fighter, game)
+    args = attr_string(fighter.attrs)
+    fighter_name = fighter.name
+    game_name = game.name
+
+    data_names  = args.keys
+    data_values = data_names.map {|k| args[k] }
+
+    tmp = []
+    data_values.each_with_index { |v,i| 
+      tmp << "t#{v},FF0000FF,0,#{i},15"
+    }
+
+    names_str =  data_names.map {|k|
+      v = args[k]
+      "#{k}"
+    }.join('|')
+
+    data_values.push(data_values[0])
+    data_str  = data_values.join(',')
+    outer_str = ([100] * data_values.size).join(',')
+    title = "#{game_name}: #{fighter_name}"
+
+    str =<<-API
+    <img src='http://chart.apis.google.com/chart?chs=250x250&cht=r&chd=t:#{data_str}|#{outer_str}&chl=#{names_str}&chm=B,FF000070,0,2,10&chtt=#{title}'></img>
+    API
+  end
 end
+
--- app/models/attr.rb
+++ app/models/attr.rb
@@ -2,7 +2,7 @@ class Attr < ActiveRecord::Base
   belongs_to :game
 
   def self.find_or_create(params)
-    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 10
+    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 4
       self.create(params)
     end
   end
