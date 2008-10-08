5051f32d7833ee28bceff36990b99c257009b430
lake <lake.ilakela+github@gmail.com>
2008-10-08 09:41:12 +0800

    亂改

--- app/models/attr.rb
+++ app/models/attr.rb
@@ -2,7 +2,7 @@ class Attr < ActiveRecord::Base
   belongs_to :game
 
   def self.find_or_create(params)
-    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 4
+    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 9
       self.create(params)
     end
   end
