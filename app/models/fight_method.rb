54c609cb2c60e5deb397af908bb4b1a55b901f49
lake <lake.ilakela+github@gmail.com>
2008-10-10 20:07:37 +0800

    style

--- app/models/fight_method.rb
+++ app/models/fight_method.rb
@@ -5,7 +5,7 @@ class FightMethod < ActiveRecord::Base
   before_create :set_default_val
 
   def set_default_val
-    self.value = self.value.zero? ? rand(200) : value
+    self.value = self.value.zero? ? rand(100) : value
   end
 
   def self.find_or_create(params)
