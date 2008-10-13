7177b7d75851263f76c86e4928c692f85f620ffc
lake <lake.ilakela+github@gmail.com>
2008-10-13 15:28:02 +0800

    add the hp limit

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -31,7 +31,7 @@ class Fighter
     PRIME_VALUE.slice(0..attr_size).each_with_index do |p_value, i|
       val = (attr_base%p_value)
       if i == 0
-        val = val < 200 ? val+200 : val
+        val = val < 250 ? val+250 : val
         #val = val * (attr_size/5)
       else
         val = val%100
