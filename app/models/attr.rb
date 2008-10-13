2536015a2cd9d1f536eb02c532e73b28a7dec1a4
lake <lake.ilakela+github@gmail.com>
2008-10-13 17:03:40 +0800

    add the custom attrs num

--- app/models/attr.rb
+++ app/models/attr.rb
@@ -3,7 +3,7 @@ class Attr < ActiveRecord::Base
   validates_length_of :name, :within => 1..5,  :too_long => "太長啦, 取短一點", :too_short => "太短啦, 取長一點"
 
   def self.find_or_create(params)
-    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 6
+    unless self.find(:first, :conditions => ["name = ?", params[:name]]) || self.count > 7
       self.create(params)
     end
   end
