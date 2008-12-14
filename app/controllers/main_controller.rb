7edba0524b45fa54015a0fc142b270b98398fe40
lake <lake.ilakela+github@gmail.com>
2008-12-14 10:35:20 +0800

    change the index graph point data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,7 @@ class MainController < ApplicationController
 
   def index
     @records = Record.find_all_wake_up_today(params[:page])
-    records = @records.size > 10 ? @records : Record.wake.find(:all, :order => 'id DESC', :limit => 10, :conditions => ["todo_time < ?", Time.now ]) 
+    records = @records.size > 7 ? @records : Record.wake.find(:all, :order => 'id DESC', :limit => 10, :conditions => ["todo_time < ?", Time.now ]) 
 
     @time = record_to_string(records)
   end
