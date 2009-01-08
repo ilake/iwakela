635351db37bf9db0761392b1141c886ea40ef7fc
lake <lake.ilakela+github@gmail.com>
2009-01-08 15:15:22 +0800

    fix path bug

--- bin/send_backup_log.rb
+++ bin/send_backup_log.rb
@@ -2,5 +2,5 @@ backup = "#{RAILS_ROOT}/log/#{RAILS_ENV}.log.template"
 source = "#{RAILS_ROOT}/log/#{RAILS_ENV}.log"
 `cp #{source} #{backup}; cat /dev/null > #{source}`
 `gzip #{backup}`
-`echo "iwakela production log" | mutt -a production.log.template.gz -s 'iwakela production log' lake.ilakela@gmail.com`
-`rm production.log.template.gz`
+`echo "iwakela production log" | mutt -a #{RAILS_ROOT}/log/production.log.template.gz -s 'iwakela production log' lake.ilakela@gmail.com`
+`rm #{RAILS_ROOT}/log/production.log.template.gz`
