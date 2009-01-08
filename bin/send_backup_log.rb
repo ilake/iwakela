d32a1b9dfad7c7f40fef5e2d789dadc09fb7bd49
lake <lake.ilakela+github@gmail.com>
2009-01-08 14:30:33 +0800

    send mail

--- bin/send_backup_log.rb
+++ bin/send_backup_log.rb
@@ -3,4 +3,4 @@ source = "#{RAILS_ROOT}/log/#{RAILS_ENV}.log"
 `cp #{source} #{backup}; cat /dev/null > #{source}`
 `gzip #{backup}`
 `echo "iwakela production log" | mutt -a production.log.template.gz -s 'iwakela production log' lake.ilakela@gmail.com`
-`rm #{backup}`
+`rm production.log.template.gz`
