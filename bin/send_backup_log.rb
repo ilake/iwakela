dc089cfc4008c964fea5b5ecc1f970a20b2c8690
lake <lake.ilakela+github@gmail.com>
2009-01-08 11:04:29 +0800

    send mail attach the production log

---
+++ bin/send_backup_log.rb
@@ -0,0 +1,6 @@
+backup = "#{RAILS_ROOT}/log/#{RAILS_ENV}.log.template"
+source = "#{RAILS_ROOT}/log/#{RAILS_ENV}.log"
+`cp #{source} #{backup}; cat /dev/null > #{source}`
+`gzip #{backup}`
+`echo "iwakela production log" | mutt -a production.log.template.gz -s 'iwakela production log' lake.ilakela@gmail.com`
+`rm #{backup}`
