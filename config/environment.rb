d8dd0156c7233fadee6ab4d882ca70e26a56211a
contagion <iwakela@clement.server0571.info>
2009-02-20 00:57:12 -0600

    check in production mode

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 ENV['RAILS_ENV'] ||= 'production'
-# ENV['HOME'] = '/home/iwakela'
+ENV['HOME'] = '/home/iwakela'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
@@ -45,7 +45,7 @@ Rails::Initializer.run do |config|
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
-  config.action_controller.session_store = :active_record_store
+  # config.action_controller.session_store = :active_record_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
@@ -62,7 +62,7 @@ end
 require "errors"
 #require 'acts_as_ferret'
 require 'smtp_tls'
-#ActionController::Base.asset_host = "iwakela.com"
+ActionController::Base.asset_host = "iwakela.com"
 ActionMailer::Base.delivery_method = :smtp 
 #ActionMailer::Base.server_settings = {
 ActionMailer::Base.smtp_settings = {
@@ -80,3 +80,10 @@ def log_to(stream)
   ActiveRecord::Base.logger = Logger.new(stream)
   ActiveRecord::Base.clear_active_connections!
 end
+
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales"
+LOCALES_AVAILABLE = Dir["#{LOCALES_DIRECTORY}/*.{rb,yml}"].collect do |locale_file|
+      I18n.load_path << locale_file
+            File.basename(File.basename(locale_file, ".rb"), ".yml")
+end.uniq.sort.reverse
+
