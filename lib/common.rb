f6f637f0a936539a23454265b8b85fcb96bec424
ilake <lake.ilakela+github@gmail.com>
2009-09-22 09:50:27 +0800

    大改版  天跟天之間的算法換成 ceil

--- lib/common.rb
+++ lib/common.rb
@@ -1,6 +1,7 @@
 module Common
   def self.cal_days_interval(time1, time2)
-    interval_days = ((time2 - time1)/86400).to_i
+    #interval_days = ((time2 - time1)/86400).to_i
+    interval_days = ((time2 - time1)/86400).ceil
     interval_days = 1 if interval_days.zero?
     interval_days
   end
