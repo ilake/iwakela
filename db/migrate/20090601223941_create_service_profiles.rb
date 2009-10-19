28949fb8246234e7dcad9c210932db61d2914a5a
ilake <lake.ilakela+github@gmail.com>
2009-10-19 14:03:45 +0800

    add service profile table back

--- db/migrate/20090601223941_create_service_profiles.rb
+++ db/migrate/20090601223941_create_service_profiles.rb
@@ -1,9 +1,9 @@
 class CreateServiceProfiles < ActiveRecord::Migration
   def self.up
-#    create_table :service_profiles do |t|
-#      t.belongs_to :user
-#      t.string :service, :name, :password
-#    end
+    create_table :service_profiles do |t|
+      t.belongs_to :user
+      t.string :service, :name, :password
+    end
   end
 
   def self.down
