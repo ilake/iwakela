52ca7db688625b0a4c717a78de7ead3bcce3b06c
ilake <lake.ilakela+github@gmail.com>
2010-03-12 17:31:31 +0800

    remove hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = 'dd6af9551efa4c48d765e9175443d733'
-  config.ignore_only  = (HoptoadNotifier::IGNORE_DEFAULT.dup << 'ActionController::UnknownAction')
-end
+#HoptoadNotifier.configure do |config|
+#  config.api_key = 'dd6af9551efa4c48d765e9175443d733'
+#  config.ignore_only  = (HoptoadNotifier::IGNORE_DEFAULT.dup << 'ActionController::UnknownAction')
+#end
